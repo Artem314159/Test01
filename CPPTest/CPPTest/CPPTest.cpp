b6789e0075e2fe1051a6501f5f5f0f999f109908
Taras Guliy <guliutaras@gmail.com>
2018-02-08 10:33:30 +0200

    Minus branch
    foo doing substration

--- CPPTest/CPPTest/CPPTest.cpp
+++ CPPTest/CPPTest/CPPTest.cpp
@@ -1,6 +1,17 @@
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
+
+int foo(int a, int b)
+{
+	return a - b;
+}
 int main()
 {
+	int plus;
+	int minus;
+	cin >> plus >> minus;
+	cout << foo(plus, minus) << endl;
     return 0;
 }
